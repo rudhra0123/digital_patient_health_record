432c03910fe9e18b4e9f52770c1a6540d824c184
rudhra0123 <22a91a0584@gmail.com>
2026-04-20 17:24:11 +0530

    fixed doctorcoll issue

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { addDoctor, getDoctors } = require("../controllers/Doctorcontroller");
-const { recordVisit, addVisitNotes } = require("../controllers/Doctorcontroller");
-const {doctorDashboard} = require("../controllers/Doctorcontroller");
+const { addDoctor, getDoctors } = require("../controllers/doctorController");
+const { recordVisit, addVisitNotes } = require("../controllers/doctorController");
+const {doctorDashboard} = require("../controllers/doctorController");
 const authMiddleware = require("../middleware/authMiddleware");
-const { getPatientProfile } = require("../controllers/Doctorcontroller");
-const {addVisit}=require("../controllers/Doctorcontroller");
+const { getPatientProfile } = require("../controllers/doctorController");
+const {addVisit}=require("../controllers/doctorController");
 
 router.get("/patient/:id", getPatientProfile);
 
